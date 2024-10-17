ad13f1712e7a0bd76de426daa2c66942621f4f9c
Raghul2510 <raghulraviworld@gmail.com>
2024-10-17 20:04:22 +0530

    portfolio init commit

---
+++ inventory/inventory/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for inventory project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+# from inventoryapp.views import GetItems,Item
+import inventoryapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('items',inventoryapp.views.GetItems.as_view(),name='get_items'),   #CBV
+    # path('item/<int:pk>',inventoryapp.views.Item.as_view(),name='item'),    #CBV
+    path('items',inventoryapp.views.GetItems,name='get_items'),
+    path('item/<int:pk>',inventoryapp.views.Item,name='item'),
+]
